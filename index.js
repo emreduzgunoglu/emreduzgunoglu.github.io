c02767743932e1c442f3d43fc3d854b12a6b8077
Emre <duzgunoglu.emre@gmail.com>
2023-09-11 02:32:36 +0300

    CSS Animations added

--- index.js
+++ index.js
@@ -40,8 +40,76 @@ function goToCall() {
     else {
         document.documentElement.scrollTop = 2800;
     }
-
 }
+
+// Course-Col
+const boxes = document.querySelectorAll('.course-col');
+window.addEventListener('scroll', checkBoxes);
+checkBoxes();
+
+function checkBoxes() {
+    const triggerBottom = window.innerHeight / 4 * 5;
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) {
+            box.classList.add('show');
+        }
+        else {
+            box.classList.remove('show');
+        }
+    })
+}
+
+// Course H1
+const courseH1 = document.querySelector('#courseHeader');
+window.addEventListener('scroll', checkCourseH1);
+checkCourseH1();
+
+function checkCourseH1() {
+    const triggerBottom = window.innerHeight;
+    const h1Top = courseH1.getBoundingClientRect().top;
+
+    if (h1Top < triggerBottom) {
+        courseH1.classList.add('show')
+    }
+    else {
+        courseH1.classList.remove('show')
+    }
+}
+
+// Course P
+const courseP = document.querySelector('#courseP')
+window.addEventListener('scroll', checkCourseP);
+checkCourseP();
+
+function checkCourseP(){
+    const triggerBottom = window.innerHeight;
+    const PTop = courseP.getBoundingClientRect().top;
+
+    if (PTop < triggerBottom) {
+        courseP.classList.add('show')
+    }
+    else {
+        courseP.classList.remove('show')
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
