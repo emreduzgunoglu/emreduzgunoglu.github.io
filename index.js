940d68be950c4814a5a60e4ef0f065822f571221
Emre <duzgunoglu.emre@gmail.com>
2023-09-10 21:26:31 +0300

    JS Functionality Updates

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 var navLinks = document.getElementById("navLinks");
 
 let closeIMGIcon = document.getElementById("closeIMG");
@@ -6,6 +8,7 @@ closeIMGIcon.addEventListener("click", hideMenu);
 let threebarsImage = document.getElementById("3bars");
 threebarsImage.addEventListener("click", showMenu);
 
+// MOBILE MODE SLIDE
 function showMenu() {
     navLinks.style.right = "0";
 }
@@ -13,6 +16,33 @@ function showMenu() {
 function hideMenu() {
     navLinks.style.right = "-200px";
 }
+
+// Header Buttons
+let callButton = document.getElementById("callBtn");
+let locationButton = document.getElementById("locationBtn");
+
+callButton.addEventListener("click", goToCall);
+locationButton.addEventListener("click", goToLocation);
+
+function goToLocation() {
+    if (screen.width > 700) {
+        document.documentElement.scrollTop = 2800;
+    }
+    else {
+        document.documentElement.scrollTop = 3300;
+    }
+}
+
+function goToCall() {
+    if (screen.width > 700) {
+        document.documentElement.scrollTop = 2100;
+    }
+    else {
+        document.documentElement.scrollTop = 2800;
+    }
+
+}
+
 
 
 
