b0c766e8a35f48b0983b26d27f6abf9476bf1484
Emre Düzgünoğlu <emreduzgunoglu0@gmail.com>
2023-09-20 17:31:52 +0300

    Collapse Button Added

--- index.js
+++ index.js
@@ -40,6 +40,23 @@ function goToCall() {
     }
 }
 
+// Collapsible
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
 /* // Course-Col
 const boxes = document.querySelectorAll('.course-col');
 window.addEventListener('scroll', checkBoxes);
